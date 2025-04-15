d4448443398d4089aacdcd02c9248405d7103f8c
rbritton3468 <44485771+rbritton3468@users.noreply.github.com>
2025-04-15 12:45:17 -0500

    Create laodManager.cxx
    loadManager.cxx

---
+++ loadManagerServer/laodManager.cxx
@@ -0,0 +1,36 @@
+#include <string>
+
+
+class Process{
+    std::string processName;
+    std::string processPath;
+    std::string processArgs;
+    std::string processStatus;
+    size_t processID;
+    size_t runTime;
+
+
+};
+
+
+
+class ProcessServer{
+    private:
+        int ServerID;
+        int processExitCode;
+
+    public:
+
+
+};
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+
+    
+    return 0;
+}
