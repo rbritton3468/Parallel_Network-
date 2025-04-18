dc7ebd3848bc90c403ec7dc2726c996ea490cd9a
rbritton3468 <44485771+rbritton3468@users.noreply.github.com>
2025-04-18 01:38:08 -0500

    Load Processor can send messages to process server via sockets

--- loadManagerServer/loadManager.cxx
+++ loadManagerServer/loadManager.cxx
@@ -1,6 +1,43 @@
 #include <string>
 #include <curl/curl.h>
 #include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+#include <list>
+#include <mutex>
+#include "loadManager.hpp"
+
+
+constexpr uint16_t ClientReceivePort = 9000;
+constexpr uint16_t InitializationPort = 9999;
+constexpr uint16_t MY_PORT = 9001;
+
+
+std::vector<ProcessServer*> processServers;
+
+
+
+class ProcessorList{
+    private:
+        std::mutex mtx;
+        std::list<std::string> list;
+    public:
+        void add(std::string item){
+            std::lock_guard<std::mutex> lock(mtx);
+            list.push_back(item);
+        }
+
+        void remove(std::string item){
+            std::lock_guard<std::mutex> lock(mtx);
+            list.remove(item);
+        }
+
+        bool contains(std::string item){
+            std::lock_guard<std::mutex> lock(mtx);
+            return std::find(list.begin(), list.end(), item) != list.end();
+        }
+
+};
 
 class Process{
     std::string processName;
@@ -11,19 +48,29 @@ class Process{
     size_t runTime;
 };
 
-std::string serverAddress;
 
 class ProcessServer{
-    private:
-        int ServerID;
-        Process* currentProcess;
-
     public:
-        std::string process(Process process){
-            // Add process to the server
+        uint16_t receivePort;
+        uint16_t sendPort;
+        std::string IPAddress;
+        boost::asio::io_context io;
+        uint16_t processServerID;
+        boost::asio::ip::udp::socket socket;
+        boost::asio::ip::udp::endpoint serverEndPoint;
+
+        void sendMessage(std::string message){
+            socket.send_to(boost::asio::buffer(message), serverEndPoint);
+
+        }
+        
+        ProcessServer(uint16_t id,std::string ip, uint16_t sendPort,uint16_t receivePort):
+        processServerID(id), IPAddress(ip), receivePort(receivePort), sendPort(sendPort),
+
+        socket(io, {boost::asio::ip::udp::v4(), receivePort}), // Properly initialize the socket
+        serverEndPoint(boost::asio::ip::make_address(IPAddress), sendPort) { // Properly initialize the endpoint
         }
 };
-
 
 class LoadManager{
     private:
@@ -33,9 +80,13 @@ class LoadManager{
         int serverPort;
     public:
         std::string addProcess(Process process){
+
+
+            // Add process to the server
             // This function will be called when a request is received from the client
             // It will parse the request and call the appropriate function
             // For example, if the request is to add a process, it will call the addProcess function
+            return "";
         }
 };
 
@@ -47,12 +98,35 @@ size_t write_callback(char *contents, size_t size, size_t nmemb, std::string *re
     return total_size;
 }
 
+void ProcessServerInitialization(){
+    uint16_t idCounter = 0;
+    boost::asio::io_context io;
+    boost::asio::ip::udp::socket InitializationSocket(io, {boost::asio::ip::udp::v4(), InitializationPort});
+
+    std::array<char, 1024> buf;
+    boost::asio::ip::udp::endpoint sender;
+    while (true)
+    {
+        auto n = InitializationSocket.receive_from(boost::asio::buffer(buf), sender);
+        std::string processServerdata = std::string(std::string_view(buf.data(), n));
+
+        std::string ip = processServerdata.substr(0, processServerdata.find('\n'));
+        std::string processServerPort = processServerdata.substr(processServerdata.find('\n')+1);
+        uint16_t processServerPortInt = std::stoi(processServerPort);
+
+        ProcessServer* processServer = new ProcessServer(idCounter,ip,processServerPortInt,9002+idCounter);
+        std::cout<<"New Process Server ID: " << idCounter << " IP: " << ip << " Port: " << processServerPort<< std::endl;
+        processServers.push_back(processServer);
+        idCounter++;
+    }
+    
+} 
+
 void processRequest(std::string request){
     // Process the request
     // This function will be called when a request is received from the client
     // It will parse the request and call the appropriate function
     // For example, if the request is to add a process, it will call the addProcess function
-
     while(true){
         // Wait for a request
         // This is a placeholder for the actual implementation
@@ -61,15 +135,26 @@ void processRequest(std::string request){
 }
 
 
-
-
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
+    if(argc < 0) {
         std::cerr << "Usage: " << argv[0] << " <server_address>"<< " <exe>" << std::endl;
         return 1;
     }
+    std::thread initializationThread(ProcessServerInitialization);
+    //boost::asio::io_context io;
+    //boost::asio::ip::udp::socket sock(io, {boost::asio::ip::udp::v4(), MY_PORT});
+    //boost::asio::ip::udp::endpoint peer(boost::asio::ip::make_address("127.0.0.1"), ClientReceivePort);
 
-    serverAddress = argv[1];
-    
+    std::string line;
+    //std::cout << "> " << std::flush;
+    while (std::getline(std::cin, line))
+    {
+    std::cout << "> " << std::flush;
+    for(ProcessServer* processServer : processServers){
+        processServer->sendMessage(line);
+    }
+
+
+}
     return 0;
 }
---
+++ processServer/processServer.cxx
@@ -0,0 +1,45 @@
+#include <string>
+#include <curl/curl.h>
+#include <iostream>
+#include <thread>
+#include <boost/asio.hpp>
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3) { std::cerr << "usage: peer_a <peer‑ip> <peer-port>\n"; return 1; }
+    std::cout<<"Process Server ID: " << argv[1] << " Port: " << argv[2] << std::endl;
+    std::string initMessage;
+    initMessage = argv[1];
+    initMessage += '\n';
+    initMessage += argv[2];
+    initMessage += '\n';
+
+    uint16_t RecevePort = std::stoi(argv[2]);
+
+    boost::asio::io_context io;
+    boost::asio::ip::udp::socket sock(io, {boost::asio::ip::udp::v4(), RecevePort});
+
+    std::array<char, 1024> buf;
+    boost::asio::ip::udp::endpoint initServer(boost::asio::ip::make_address("127.0.0.1"), 9999);
+
+
+    sock.send_to(boost::asio::buffer(initMessage), initServer);
+
+    boost::asio::ip::udp::endpoint sender(boost::asio::ip::make_address("127.0.0.1"), RecevePort);
+    for (;;){
+        auto n = sock.receive_from(boost::asio::buffer(buf), sender);
+        
+        std::cout << "\n<managerServer> " << std::string_view(buf.data(), n) << '\n';
+
+    }
+
+
+
+
+
+}
