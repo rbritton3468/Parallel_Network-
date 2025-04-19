a9b60b3c302e820354c7bec37a68a8be69f8fd89
Yash Bingi <yashbingi@wireless-10-148-83-203.public.utexas.edu>
2025-04-19 17:00:46 -0500

    Add:fileCompression and test files

---
+++ fileCompression/compression.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+#include <vector>
+
+std::string compressFile(const std::string& inputExe);
+
+void decompressFile(const std::string& compressedFile);
---
+++ fileCompression/main.cpp
@@ -0,0 +1,9 @@
+#include "compression.hpp"
+// #include "decompressor.h"
+#include <string>
+#include <iostream>
+
+int main(){
+    compressFile("testFile");
+    return 0;
+}
